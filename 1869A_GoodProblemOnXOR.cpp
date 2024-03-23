b6880ad6f25427b584a9e9d5cae2f64e26883225
Shivam08IITKGP <124419114+Shivam08IITKGP@users.noreply.github.com>
2024-03-23 10:27:44 +0530

    Create 1869A_GoodProblemOnXOR.cpp

---
+++ 1869A_GoodProblemOnXOR.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FAST                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+typedef long long int ll;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    if (n & 1)
+    {
+        cout << 4 << endl;
+        cout << 1 << " " << n - 1 << endl;
+        cout << 1 << " " << n - 1 << endl;
+        cout << n - 1 << " " << n << endl;
+        cout << n - 1 << " " << n << endl;
+    }
+    else
+    {
+        cout << 2 << endl;
+        cout << 1 << " " << n << endl;
+        cout << 1 << " " << n << endl;
+    }
+    return;
+}
+
+int main()
+{
+    FAST;
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
