25e0195f548eeece0226f12583e642f019ea5a75
Shivam Choudhury <124419114+Shivam08IITKGP@users.noreply.github.com>
2024-06-11 13:21:25 +0530

    Create 1775B_BitwiseORSubsequencArray.cpp

---
+++ 1775B_BitwiseORSubsequencArray.cpp
@@ -0,0 +1,45 @@
+// Simply check if in every number there is a bit that is unique to only that number
+// If every number has such a unique bit, then answer is no
+// Else YES
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    vi bits[n];
+    map<int,int>m;
+    for (int i = 0; i < n; i++)
+    {
+        int k;
+        read(k);
+        while (k--)
+        {
+            int x;
+            cin >> x;
+            bits[i].pb(x);
+            m[x]++;
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        bool present= false;
+        for(auto j:bits[i])
+        {
+            if(m[j]==1)
+            {
+                present= true;
+            }
+        }
+        if(!present)
+        {
+            cout<<"YES\n";
+            return;
+        }
+
+
+    }
+    cout<<"NO\n";
+    return;
+}
