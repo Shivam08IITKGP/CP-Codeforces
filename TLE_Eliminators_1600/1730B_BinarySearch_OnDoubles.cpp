283475f64325a42a4e375f2c6eb66994351170a6
Shivam Choudhury <124419114+Shivam08IITKGP@users.noreply.github.com>
2024-06-20 00:01:40 +0530

    Cout<<setprecision(14);

--- TLE_Eliminators_1600/1730B_BinarySearch_OnDoubles.cpp
+++ TLE_Eliminators_1600/1730B_BinarySearch_OnDoubles.cpp
@@ -56,3 +56,16 @@ void solve()
     }
     cout << maxLeft << endl;
 }
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t = 1;
+    cin >> t;
+    cout << setprecision(14);
+    while (t--)
+    {
+        solve();
+    }
+}
