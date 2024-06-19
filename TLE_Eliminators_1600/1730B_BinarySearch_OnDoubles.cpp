d60c87eaff8c1ee1efc41e6d72a40a94a377a3de
Shivam Choudhury <124419114+Shivam08IITKGP@users.noreply.github.com>
2024-06-19 23:59:48 +0530

    Create 1730B_BinarySearch_OnDoubles.cpp

---
+++ TLE_Eliminators_1600/1730B_BinarySearch_OnDoubles.cpp
@@ -0,0 +1,58 @@
+const long double prec = 1e-6;
+
+#define ld long double
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<ld> t(n), x(n);
+    read(x);
+    read(t);
+    if (n == 1)
+    {
+        cout << x[0] << endl;
+        return;
+    }
+    ld l = -1, r = 1e9;
+
+    auto check = [&](ld T) -> bool
+    {
+        ld minRight = 1e9;
+        ld maxLeft = -1;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (T < t[i])
+            {
+                return false;
+            }
+            // T>t[i]
+            remin(minRight, x[i] + T - t[i]);
+            remax(maxLeft, x[i] - T + t[i]);
+        }
+        return (maxLeft <= minRight);
+    };
+
+    // int ti;
+    while (r - l > prec)
+    {
+        ld T = (l + r) / 2.0;
+        if (check(T))
+        {
+            r = T;
+        }
+        else
+        {
+            l = T;
+        }
+    }
+
+    ld minRight = 1e9, maxLeft = -1;
+    for (int i = 0; i < n; i++)
+    {
+        remin(minRight, x[i] + r - t[i]);
+        remax(maxLeft, x[i] - r + t[i]);
+    }
+    cout << maxLeft << endl;
+}
